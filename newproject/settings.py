839081636571f5a6c9ef35dd2a689041a00503de
KingSeraph07 <runsewemichael93@gmail.com>
2025-08-08 14:18:54 +0100

    update

--- newproject/settings.py
+++ newproject/settings.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS settings
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    # "https://your-production-domain.com",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     # "https://your-production-domain.com",
+# ]
